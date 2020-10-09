731d3c29d5165418957caddc0258c8858267d58c
Luca Ventrice <luca@ventrice.ch>
2020-10-09 15:49:22 +0200

    add base & detail tempalte, URL linking

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,10 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 from projects.models import Project
 
 def all_projects(request):
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
