0fec0cd8396e636a285f024c62527e51fa0715cf
Luca Ventrice <luca@ventrice.ch>
2020-10-09 14:02:40 +0200

    create a template

--- projects/views.py
+++ projects/views.py
@@ -2,4 +2,4 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def project_list(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, 'projects/index.html')
