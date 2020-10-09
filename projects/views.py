f258b216deae750ae3fca7af38ed5048ffb667a6
Luca Ventrice <luca@ventrice.ch>
2020-10-09 13:54:30 +0200

    create initial test view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,5 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def project_list(request):
+    return HttpResponse("<h1>Hello World!</h1>")
