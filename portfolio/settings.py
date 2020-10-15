7138484dc8fb9e3cfbc1df044de5580c2ea89ef7
Luca Ventrice <luca@ventrice.ch>
2020-10-15 12:38:56 +0200

    fix static img paths

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR + "/projects/static",
+]
 
 # Login
 LOGIN_REDIRECT_URL = "users_dashboard"
